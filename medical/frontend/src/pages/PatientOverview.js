8b968346b5025f90c87c4c10e30c8e670e1c809c
Luigi <luigiaubrypouget@gmail.com>
2024-06-04 10:02:30 +0200

    ajout des pages générales front med

---
+++ medical/frontend/src/pages/PatientOverview.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Patient = () => {
+    return (
+        <div>
+            PatientOverview page
+        </div>
+    );
+};
+
+export default Patient;
---
+++ medical/frontend/src/pages/PatientRegister.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PatientRegister = () => {
+    return (
+        <div>
+            PatientRegister page
+        </div>
+    );
+};
+
+export default PatientRegister;

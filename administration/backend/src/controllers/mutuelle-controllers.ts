8285552d864ede3c900dfd82ba28d669a4271f7e
issam <issame.hassane@hotmail.com>
2024-06-23 18:56:58 +0200

    correction ajout liste mutuelles

--- administration/backend/src/controllers/mutuelle-controllers.ts
+++ administration/backend/src/controllers/mutuelle-controllers.ts
@@ -15,18 +15,18 @@ const mutuelleGetAll = async ( request: express.Request,
 
         // Exécuter une requête SQL
         const [rows] = await connection.execute<RowDataPacket[]>('SELECT * FROM mutuelle');
-        let mutuelles = new Set<IMutuelle>();
+        //let mutuelles = new Set<IMutuelle>();
         if (rows.length === 0) {
             // Si aucun résultat n'est trouvé, renvoyer une erreur 404
             return response.status(404).json({ message: 'Aucune mutuelle trouvé' });
         }
-        for (let i = 0; i < rows.length; i++) {
-            mutuelles.add(rowToIMutuelle(rows[i]));
-            console.log(mutuelles);
-        }
+        //for (let i = 0; i < rows.length; i++) {
+        //   mutuelles.add(rowToIMutuelle(rows[i]));
+        //    console.log(mutuelles);
+        //}
         // Libérer la connexion
         connection.release();
-        response.json(mutuelles);
+        response.json(rows);
     } catch (error) {
         console.error('Erreur lors de la récupération des mutuelles:', error);
         response.status(500).json({ message: 'Erreur serveur' });
